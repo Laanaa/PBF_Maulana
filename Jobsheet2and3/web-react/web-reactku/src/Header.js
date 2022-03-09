b01e0b7f398021e7d5579707ccf919acc8da9a8a
Laanaa <maulanamalik156357n@gmail.com>
2022-03-09 19:14:42 +0700

    Jobsheet3

---
+++ Jobsheet2and3/web-react/web-reactku/src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            <h3>Halaman Header</h3>
+            <h3>Component ini dibuat menggunakan Function bukan Class</h3>
+        </div>
+    )
+}
+
+export default Header;

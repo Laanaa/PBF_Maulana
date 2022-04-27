b55195fb02ca6b6d8aa0988015793bccd57e5212
Laanaa <maulanamalik156357n@gmail.com>
2022-04-28 00:01:13 +0700

    Update jobsheet

---
+++ jobsheet10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDSQABZHZlF6W60X-Q6SW-dcIVC759O_VE",
+    authDomain: "reactpbf.firebaseapp.com",
+    projectId: "reactpbf",
+    storageBucket: "reactpbf.appspot.com",
+    messagingSenderId: "1030852580335",
+    appId: "1:1030852580335:web:00913caef03d1362485c1a",
+    measurementId: "G-17WCT2JVQS"
+}
